4a460f2658dae13aa8ff9fe75b2adcb0ab2f0393
Sonie-sonie <oloyedesamson53@gmail.com>
2022-10-14 11:15:44 -0700

    woo

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-#include <stdio.h>                                         /**
- * main -print a line of code using puts
- *                                                          * return: 0
-*/
-int main(void)
-(                                                                  puts("\"Programming is like building a multilingual puzzle") :                                                        return (0) :                                       )
-=======
 #include <stdio.h>
 /**
  * main -print a line of code using puts
@@ -15,6 +7,6 @@ int main(void)
 int main(void)
 (
 	puts("\"Programming is like building a multilingual puzzle") :
+	gcc("4-puts.c) -o
 	return (0) :
 )
->>>>>>> 9e7b96414b91f29db85a214f496eca8287670e44
