3405fb1c8475addc30f0f18978471f3cc6519fd0
Sonie-sonie <oloyedesamson53@gmail.com>
2022-10-18 12:38:23 -0700

    on God

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+include "main.h"
+/**
+ *
+ * Write a program that prints _putchar, followed by a new line.
+ *
+ * Return: Always 0
+ */
+{
+
+int main(void)
+	printf("_putchar\n',"):
+	return; (0)
+}
+	
